4691e805e7c4a268dbe392302b9095a68782f0cd
aboba <pomidkil@gmail.com>
2025-07-16 22:37:37 +0300

    Колода для карт

--- KingOfTheHill/Models.cs
+++ KingOfTheHill/Models.cs
@@ -29,13 +29,45 @@ public class BonusCard : ICard
     public int Invoke(Player player) => player.Score * 2;
 }
 
-public class SpecialCard(SpecialCommand command)
+public class SpecialCard(SpecialCommand command) : ICard
 {
     public SpecialCommand Value = command;
 
     public void Invoke(Player player)
     {
         throw new NotImplementedException();
+    }
+}
+
+public static class CardDeck
+{
+    private static readonly Random random = new();
+    
+    public static ICard DrawRandomCard()
+    {
+        double chance = random.NextDouble(); // 0.0 - 1.0
+        
+        if (chance < 0.8) // 80% обычные карты
+        {
+            bool isPositive = random.Next(2) == 0; // 50/50 позитивная/негативная
+            int value = random.Next(1, 11); // Значение 1-10
+            
+            return isPositive 
+                ? new PositiveCard(value) 
+                : new NegativeCard(value);
+        }
+        else if (chance < 0.95) // 15% специальные карты (0.8-0.95)
+        {
+            var command = random.Next(2) == 0 
+                ? SpecialCommand.Silence 
+                : SpecialCommand.ChangeMove;
+                
+            return new SpecialCard(command);
+        }
+        else // 5% бонусные карты (0.95-1.0)
+        {
+            return new BonusCard();
+        }
     }
 }
 
